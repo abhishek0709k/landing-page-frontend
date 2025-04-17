aa0a1d826b8a5aa9e89a972ab1a28ca9861deac3
abhishek0709k <abhishekkumarsingh844141@gmail.com>
2025-04-17 20:01:39 +0530

    add backend url

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./ContactUs.css";
 import { Link } from "react-router";
 import Navbar from "../navbar/Navbar";
-import axios from "axios"
+import axios from "axios";
 const ContactUs = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -18,14 +18,16 @@ const ContactUs = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/contact-us", formData);
-      console.log(response)
-      setSubmitted(true)
+      const response = await axios.post(
+        "https://landing-project-backend.onrender.com/contact-us",
+        formData
+      );
+      console.log(response);
+      setSubmitted(true);
     } catch (error) {
       console.error("Request failed:", error);
     }
   };
-  
 
   return (
     <>
@@ -37,7 +39,9 @@ const ContactUs = () => {
             <p className="thank-you-msg">
               Thanks for reaching out! We'll get back to you soon.
             </p>
-            <Link to={"/"} className="home-button">Home</Link>
+            <Link to={"/"} className="home-button">
+              Home
+            </Link>
           </>
         ) : (
           <form className="contact-form" onSubmit={handleSubmit}>
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
-import './Signup.css';
-import { useNavigate } from 'react-router';
-import Navbar from '../navbar/Navbar';
+import React, { useState } from "react";
+import "./Signup.css";
+import { useNavigate } from "react-router";
+import Navbar from "../navbar/Navbar";
 const Signup = () => {
-  const navigate = useNavigate()
-  const [formData, setFormData] = useState({ name: '', email: '', skill: '', portfolio: '' });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    skill: "",
+    portfolio: "",
+  });
   const [submitted, setSubmitted] = useState(false);
 
   const handleChange = (e) => {
@@ -13,30 +18,51 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.name || !formData.email || !formData.skill) return alert('Please fill in required fields');
-    await fetch('http://localhost:5000/api/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    if (!formData.name || !formData.email || !formData.skill)
+      return alert("Please fill in required fields");
+    await fetch("https://landing-project-backend.onrender.com/api/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     });
     setSubmitted(true);
-    navigate("/")
+    navigate("/");
   };
 
   return submitted ? (
     <h2>Thank you for signing up!</h2>
   ) : (
     <>
-    <Navbar/>
-    <form className="signup-form" onSubmit={handleSubmit}>
-    <h2>Sign Up</h2>
-      <input name="name" placeholder="Name" onChange={handleChange} required />
-      <input name="email" placeholder="Email" type="email" onChange={handleChange} required />
-      <input name="skill" placeholder="Skill Category" onChange={handleChange} required />
-      <input name="portfolio" placeholder="Portfolio Link" onChange={handleChange} />
-      <button type="submit">Sign Up</button>
-      <p>Sign Up to continue with site...</p>
-    </form>
+      <Navbar />
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <input
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          type="email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="skill"
+          placeholder="Skill Category"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="portfolio"
+          placeholder="Portfolio Link"
+          onChange={handleChange}
+        />
+        <button type="submit">Sign Up</button>
+        <p>Sign Up to continue with site...</p>
+      </form>
     </>
   );
 };
